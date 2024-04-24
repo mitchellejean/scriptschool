c2909caa3c231de13ca9f0d6441d0e3a876b4f77
mitchellejean <chebetmitchellejean@gmail.com>
2024-04-24 08:54:10 +0300

    first commit

---
+++ studentmanagement/seriolizers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from Studentmanagement.models import Student
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
